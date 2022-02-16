ff0b5cd41e6926d95ebe90414c0c19618f6aca76
harsona <imharsona@gmail.com>
2022-02-16 23:58:16 +0900

    Step16. MVC 기반 웹 게시판 만들기

--- src/main/webapp/resources/sql/board.sql
+++ src/main/webapp/resources/sql/board.sql
@@ -1,12 +1,12 @@
 CREATE TABLE board (
-       num int not null auto_increment,
-       id varchar(10) not null,
-       name varchar(10) not null,
-       subject varchar(100) not null,
-       content text not null,
-       regist_day varchar(30),
-       hit int,
-       ip varchar(20),
+       num int not null auto_increment, //게시글 번호
+       id varchar(10) not null,	//회원아이디
+       name varchar(10) not null, //회원이름
+       subject varchar(100) not null, //게시글 제목
+       content text not null, //게시글 내용
+       regist_day varchar(30), //게시글 등록일자
+       hit int, //게시글 조회수
+       ip varchar(20), //게시글 등록 ip
        PRIMARY KEY (num)
 )default CHARSET=utf8;
 
